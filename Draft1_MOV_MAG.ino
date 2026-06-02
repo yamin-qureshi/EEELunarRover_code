69eb7180151e8225bfb1d42a0d52e7c2723b2c4d
yamin-qureshi <yq1925@ic.ac.uk>
2026-06-02 16:17:35 +0100

    Update Draft1_MOV_MAG.ino

--- Draft1_MOV_MAG.ino
+++ Draft1_MOV_MAG.ino
@@ -1,3 +1,4 @@
+// this code combines the original movement component with the magnetometer reading
 #define USE_WIFI_NINA         false
 #define USE_WIFI101           true
 #include <WiFiWebServer.h>
